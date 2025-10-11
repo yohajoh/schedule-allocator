04158e9a0096d70adf8ed99aaa14d3658f414c3a
yohannes belete <john@kali.kali>
2025-10-11 04:32:22 -0400

    full structured setup

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div className="text-center mt-30">
+      <p className="text-2xl text-red-500">Something went wrong :(</p>
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="text-center mt-30">
+      <p className="text-2xl">Page not found in this url :(</p>
+    </div>
+  );
+}
